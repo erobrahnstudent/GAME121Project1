b453bd6144fb388c2c8eb7c6277a13015f77e023
Edward Robrahn <erobrahn@stumail.jccc.edu>
2018-02-21 16:14:30 -0600

    Couple small changes since there was an exercise today- Jump added and movement up the ramp is smooth. Going to look at making a new level for this.

--- SimpleShooter/Assets/Scripts/FirstPersonControls.cs
+++ SimpleShooter/Assets/Scripts/FirstPersonControls.cs
@@ -4,32 +4,76 @@ using UnityEngine;
 // Author: Edward Robrahn
 public class FirstPersonControls : MonoBehaviour {
     public float speed = 10.0f;
+    public float airspeed = 5.0f;
     public float sensitivityX = 10.0f;
+    public float gravity = 5f;
+    public float groundedgrav = 0.75f;
+    
+    public CharacterController playerController;
 
+    public float jumpSpeed = 10;
+    float verticalVelocity;
+    public float velocityDecay = 5f;
 	void Start () {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 	}
 	
 	void Update () {
-        Vector3 moveDirection = Vector3.zero;
-		if (Input.GetKey(KeyCode.W))
+        if (playerController.isGrounded)
         {
-            moveDirection += transform.forward;
+            if (verticalVelocity < 0) verticalVelocity = 0;
+            Vector3 moveDirection = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveDirection += transform.forward;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveDirection += -transform.forward;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveDirection += -transform.right;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveDirection += transform.right;
+            }
+            moveDirection.Normalize();
+            if (Input.GetKey(KeyCode.Space) && verticalVelocity == 0)
+            {
+                verticalVelocity = jumpSpeed;
+                moveDirection.y += verticalVelocity;
+            }
+            playerController.Move(moveDirection * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.S))
+        else
         {
-            moveDirection += -transform.forward;
+            Vector3 moveDirection = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveDirection += transform.forward;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveDirection += -transform.forward;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveDirection += -transform.right;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveDirection += transform.right;
+            }
+            moveDirection.Normalize();
+            verticalVelocity -= velocityDecay * Time.deltaTime;
+            moveDirection.y += verticalVelocity;
+            playerController.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveDirection += -transform.right;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveDirection += transform.right;
-        }
-        transform.position += moveDirection.normalized * speed * Time.deltaTime;
+        // depreciated transform:
+        // transform.position += moveDirection.normalized * speed * Time.deltaTime;
 
         if (Input.GetAxis("Mouse X") > 0.002f || Input.GetAxis("Mouse X") < 0.002f)
         {
